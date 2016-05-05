6d66d0ff59da71ee85f7b6072090fd489f153e8a
Junji Watanabe <watanabe0621@gmail.com>
2016-05-05 18:47:06 +0900

    tutorial

--- build.sbt
+++ build.sbt
@@ -1,12 +1,9 @@
-name := "Simple Project"
+name := "Simple Spark Project"
 
 version := "1.0"
 
 scalaVersion := "2.10.5"
 
-val sparkVersion = "1.6.1"
-
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % sparkVersion
+  "org.apache.spark" %% "spark-core" % "1.6.1"
 )
-
--- src/main/scala/SimpleSparkApp.scala
+++ src/main/scala/SimpleSparkApp.scala
@@ -1,6 +1,13 @@
+import org.apache.spark.{SparkContext, SparkConf}
 
 object SimpleSparkApp {
   def main(args: Array[String]) = {
-    println("Hello World, This is simple spark app!!")
+    val conf = new SparkConf().setAppName("Simple Spark Application")
+    val sc = new SparkContext(conf)
+    val txtFile = "sample.txt"
+    val txtData = sc.textFile(txtFile, 2).cache()
+    val numAs = txtData.filter(line => line.contains("a")).count()
+    val numBs = txtData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
   }
 }
